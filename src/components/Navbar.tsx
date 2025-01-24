3eccba330f3f6926373f0fbd89623405c1b6cbbf
SumanSingha05 <sumansingha8654@gmail.com>
2025-01-24 22:36:23 +0530

    paragraph added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,9 +19,18 @@ function Navbar({ className }: { className?: string }) {
         </Link>
         <MenuItem setActive={setActive} active={active} item="Our Courses">
         <div className="flex flex-col space-y-4 text-sm">
+
         <HoveredLink href="/courses">All courses</HoveredLink>
+        <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+        <HoveredLink href="/courses">Composition</HoveredLink>
+        <HoveredLink href="/courses">Songwriting</HoveredLink>
+        <HoveredLink href="/courses">Music Production </HoveredLink>
         </div>
         </MenuItem>
+        <Link href={"/contact"}>
+        <MenuItem setActive={setActive} active={active} item="Contact Us">
+        </MenuItem>
+        </Link>
       </Menu>
     </div>
   )
---
+++ src/components/ui/HeroSection.tsx
@@ -0,0 +1,13 @@
+
+const HeroSection = () => {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <div className="p-4 relative z-10 w-full text-center">
+        <h1>Master the art of Music</h1>
+        <p>Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+    </div>    
+        </div>
+  )
+}
+
+export default HeroSection
